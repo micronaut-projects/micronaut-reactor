d6591bd85275e206671d0e1676be8ab9af9f2378
Sergio del Amo <sergio.delamo@softamo.com>
2021-07-14 12:11:12 +0200

    test: remove dead code in TraceInterceptorSpec

--- reactor-http-client/src/test/groovy/io/micronaut/reactor/http/client/TraceInterceptorSpec.groovy
+++ reactor-http-client/src/test/groovy/io/micronaut/reactor/http/client/TraceInterceptorSpec.groovy
@@ -2,17 +2,17 @@ package io.micronaut.reactor.http.client
 
 import brave.SpanCustomizer
 import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
 import io.micronaut.core.util.StringUtils
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
-import io.micronaut.tracing.annotation.ContinueSpan
 import io.micronaut.tracing.annotation.NewSpan
 import io.micronaut.tracing.annotation.SpanTag
-import io.reactivex.Single
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 import spock.lang.Specification
 import spock.util.concurrent.PollingConditions
-
+import zipkin2.Span
+import zipkin2.reporter.Reporter
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
@@ -20,12 +20,11 @@ import jakarta.inject.Singleton
 @Property(name = 'tracing.zipkin.enabled', value = StringUtils.TRUE)
 @Property(name = 'tracing.zipkin.sampler.probability', value = "1")
 @Property(name = 'tracing.instrument-threads', value = StringUtils.TRUE)
+@Property(name = 'spec.name', value = "TraceInterceptorSpec")
 class TraceInterceptorSpec extends Specification {
 
     @Inject TracedService tracedService
     @Inject TestReporter reporter
-
-
 
     void "test trace mono"() {
         when:
@@ -43,24 +42,11 @@ class TraceInterceptorSpec extends Specification {
         }
     }
 
+    @Requires(property = 'spec.name', value = "TraceInterceptorSpec")
     @Singleton
     static class TracedService {
 
         @Inject SpanCustomizer spanCustomizer
-        @NewSpan("my-trace")
-        String methodOne(@SpanTag("foo.bar") String name) {
-            methodTwo(name)
-        }
-
-        @ContinueSpan
-        String methodTwo(@SpanTag("foo.baz") String another) {
-            methodThree(another).blockingGet()
-        }
-
-        @NewSpan("trace-rx")
-        Single<String> methodThree(@SpanTag("more.stuff") String name) {
-            return Single.just(name)
-        }
 
         @NewSpan("trace-mono")
         Mono<String> mono(@SpanTag("more.stuff") String name) {
@@ -69,23 +55,15 @@ class TraceInterceptorSpec extends Specification {
                 return name
             }).subscribeOn(Schedulers.elastic())
         }
-
-
     }
-}
 
-import zipkin2.Span
-import zipkin2.reporter.Reporter
-
-/**
- * @author graemerocher
- * @since 1.0
- */
-@Singleton
-class TestReporter implements Reporter<Span> {
-    List<Span> spans = []
-    @Override
-    void report(Span span) {
-        spans.add(span)
+    @Requires(property = 'spec.name', value = "TraceInterceptorSpec")
+    @Singleton
+    static class TestReporter implements Reporter<Span> {
+        List<Span> spans = []
+        @Override
+        void report(Span span) {
+            spans.add(span)
+        }
     }
 }
