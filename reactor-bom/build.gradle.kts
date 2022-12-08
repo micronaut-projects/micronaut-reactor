a81c1aa277b713e1406b30caf72978255d127df8
Tim Yates <tim.yates@gmail.com>
2022-12-08 11:05:24 +0000

    Switch to declaring in the catalog

--- reactor-bom/build.gradle.kts
+++ reactor-bom/build.gradle.kts
@@ -4,15 +4,7 @@ plugins {
 micronautBom {
     suppressions {
         bomAuthorizedGroupIds.put("io.projectreactor:reactor-bom", setOf("org.reactivestreams"))
-        acceptedVersionRegressions.addAll("reactor", "reactor-compat")
-        acceptedLibraryRegressions.addAll("reactor", "reactor-core", "reactor-test")
-    }
-    catalog {
-        // We want to be able to reference these versions from other modules that import the catalog of this project
-        versionCatalog {
-            version("reactor", "3.5.0")
-            library("reactor-core", "io.projectreactor", "reactor-core").versionRef("reactor")
-            library("reactor-test", "io.projectreactor", "reactor-test").versionRef("reactor")
-        }
+        acceptedVersionRegressions.addAll("reactor-compat")
+        acceptedLibraryRegressions.addAll("reactor")
     }
 }
