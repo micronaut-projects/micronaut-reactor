1c061fdad7c3556acadffbd2734635af4fe2f467
Denis Stepanov <denis.stepanov@gmail.com>
2023-05-30 09:24:33 -0500

    Use new propagation API (#292)

--- reactor/src/main/java/io/micronaut/reactor/instrument/ReactorInstrumentation.java
+++ reactor/src/main/java/io/micronaut/reactor/instrument/ReactorInstrumentation.java
@@ -17,22 +17,12 @@ package io.micronaut.reactor.instrument;
 
 import io.micronaut.context.annotation.Context;
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.env.Environment;
 import io.micronaut.core.annotation.Internal;
-import io.micronaut.core.annotation.Nullable;
-import io.micronaut.core.util.CollectionUtils;
-import io.micronaut.scheduling.instrument.Instrumentation;
-import io.micronaut.scheduling.instrument.InvocationInstrumenter;
-import io.micronaut.scheduling.instrument.ReactiveInvocationInstrumenterFactory;
+import io.micronaut.core.propagation.PropagatedContext;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Hooks;
-import reactor.core.publisher.Operators;
 import reactor.core.scheduler.Schedulers;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Instruments Reactor such that the thread factory used by Micronaut is used and instrumentations can be applied to the {@link java.util.concurrent.ScheduledExecutorService}.
@@ -41,102 +31,20 @@ import java.util.List;
  * @since 1.0
  */
 @Requires(sdk = Requires.Sdk.MICRONAUT, version = "2.0.0")
-@Requires(classes = {Flux.class, Schedulers.Factory.class})
+@Requires(classes = {Flux.class, Schedulers.Factory.class, PropagatedContext.class})
 @Context
 @Internal
-class ReactorInstrumentation {
-    /**
-     * Initialize instrumentation for reactor with the tracer and factory.
-     *
-     * @param instrumenterFactory The instrumenter factory
-     */
+final class ReactorInstrumentation {
+
+    private static final String KEY = "MICRONAUT_CONTEXT_PROPAGATION";
+
     @PostConstruct
-    void init(ReactorInstrumenterFactory instrumenterFactory) {
-        if (instrumenterFactory.hasInstrumenters()) {
-            Schedulers.onScheduleHook(Environment.MICRONAUT, runnable -> {
-                InvocationInstrumenter instrumenter = instrumenterFactory.create();
-                if (instrumenter != null) {
-                    return () -> {
-                        try (Instrumentation ignored = instrumenter.newInstrumentation()) {
-                            runnable.run();
-                        }
-                    };
-                }
-                return runnable;
-            });
-            Hooks.onEachOperator(Environment.MICRONAUT, Operators.lift((scannable, coreSubscriber) -> {
-                if (coreSubscriber instanceof ReactorSubscriber) {
-                    return coreSubscriber;
-                }
-                InvocationInstrumenter instrumenter = instrumenterFactory.create();
-                if (instrumenter != null) {
-                    return new ReactorSubscriber<>(instrumenter, coreSubscriber);
-                }
-                return coreSubscriber;
-            }));
-        }
+    void init() {
+        Schedulers.onScheduleHook(KEY, PropagatedContext::wrapCurrent);
     }
 
-    /**
-     * Removes the registered instrumentation.
-     */
     @PreDestroy
     void removeInstrumentation() {
-        Schedulers.removeExecutorServiceDecorator(Environment.MICRONAUT);
-        Hooks.resetOnEachOperator(Environment.MICRONAUT);
-    }
-
-
-    @Context
-    @Requires(classes = Flux.class)
-    @Internal
-    static final class ReactorInstrumenterFactory {
-
-        private final List<ReactiveInvocationInstrumenterFactory> reactiveInvocationInstrumenterFactories;
-
-        /**
-         * @param reactiveInvocationInstrumenterFactories invocation instrumenters
-         */
-        ReactorInstrumenterFactory(List<ReactiveInvocationInstrumenterFactory> reactiveInvocationInstrumenterFactories) {
-            this.reactiveInvocationInstrumenterFactories = reactiveInvocationInstrumenterFactories;
-        }
-
-        /**
-         * Check if there are any instumenters present.
-         *
-         * @return true if there are any instumenters present
-         */
-        public boolean hasInstrumenters() {
-            return !reactiveInvocationInstrumenterFactories.isEmpty();
-        }
-
-        /**
-         * Created a new {@link InvocationInstrumenter}.
-         *
-         * @return new {@link InvocationInstrumenter} if instrumentation is required
-         */
-        @Nullable
-        public InvocationInstrumenter create() {
-            List<InvocationInstrumenter> invocationInstrumenter = getReactiveInvocationInstrumenters();
-            if (CollectionUtils.isNotEmpty(invocationInstrumenter)) {
-                return InvocationInstrumenter.combine(invocationInstrumenter);
-            }
-            return null;
-        }
-
-        /**
-         * @return The invocation instrumenters
-         */
-        private List<InvocationInstrumenter> getReactiveInvocationInstrumenters() {
-            List<InvocationInstrumenter> instrumenters = new ArrayList<>(reactiveInvocationInstrumenterFactories.size());
-            for (ReactiveInvocationInstrumenterFactory instrumenterFactory : reactiveInvocationInstrumenterFactories) {
-                final InvocationInstrumenter instrumenter = instrumenterFactory.newReactiveInvocationInstrumenter();
-                if (instrumenter != null) {
-                    instrumenters.add(instrumenter);
-                }
-            }
-            return instrumenters;
-        }
-
+        Schedulers.resetOnScheduleHook(KEY);
     }
 }
